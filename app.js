1c37bad5f002b0cf074b845d9ca2945913747e19
Mahmoud Ahmed <64172597+Mahmoud-D@users.noreply.github.com>
2023-01-09 22:33:15 +0200

    first-version

---
+++ app.js
@@ -0,0 +1,20 @@
+let dark =document.querySelector('.dark')
+let night =document.querySelector('.night')
+let light =document.querySelector('.light')
+let sidebar =document.querySelector('.sidebar')
+let activeListItem = document.querySelector('.list-item.active')
+
+dark.addEventListener('click',()=>{
+    sidebar.className="sidebar";
+    activeListItem='list-item active'
+})
+night.addEventListener('click',()=>{
+    sidebar.className="sidebar night";
+    activeListItem='list-item active night'
+})
+light.addEventListener('click',()=>{
+    sidebar.className="sidebar light";
+    activeListItem='list-item active light'
+})
+console.log(activeListItem)
+console.log(night)
